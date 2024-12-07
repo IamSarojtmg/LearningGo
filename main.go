496bea1b52465544b524b4f8fb6360b4f530ec7f
Saroj Tamang <sarojtmg992@gmail.com>
2024-12-07 09:56:21 +0000

    map

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
 
-import (
-	"html/template"
-	"log"
-	"net/http"
-)
+import "fmt"
 
-type MoreInfo struct {
-	Topic       string
-	Description string
-}
 
-func moreInfo() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		temp := template.Must(template.ParseFiles("index.html"))
-		temp.Execute(w, nil)
-	})
-
-	http.HandleFunc("/moreinfo", func(w http.ResponseWriter, r *http.Request) {
-		moreInfos := []MoreInfo{
-
-			{Topic: "Basketball", Description: "Basketball is a team sport in which two teams, most commonly of five players each, opposing one another on a rectangular court, compete with the primary objective of shooting a basketball (approximately 9.4 inches (24 cm) in diameter) through the defender's hoop (a basket 18 inches (46 cm) in diameter mounted 10 feet (3.048 m) high to a backboard at each end of the court), while preventing the opposing team from shooting through their own hoop."},
-		}
-		moreInfoTemplate := `
-			{{range .}}
-				<li>{{.Topic}}: {{.Description}}</li>
-			{{end}}
-
-	`
-
-		temp := template.Must(template.New("moreInfo").Parse(moreInfoTemplate))
-		temp.Execute(w, moreInfos)
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
 
 func main() {
-	moreInfo()
+var mp map[string]int = map[string]int{
+"liverpool": 1,
+"chelsea":2,
+"arsenal":3,
+}
+
+fmt.Println(mp)
+ 
 }
